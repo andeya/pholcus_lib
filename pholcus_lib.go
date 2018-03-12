28cf38265ddee3c93515d4bbf5b6f51d92e2a5ba
刘刚 <liugang5@staff.weibo.com>
2018-03-12 10:50:13 +0800

    import 没导入

--- pholcus_lib.go
+++ pholcus_lib.go
@@ -29,5 +29,6 @@ import (
 	_ "github.com/henrylee2cn/pholcus_lib/zolpc"
 	_ "github.com/henrylee2cn/pholcus_lib/zolphone"
 	_ "github.com/henrylee2cn/pholcus_lib/zolslab"
-	_ "github.com/henrylee2cn/pholcus_lib/zhihu"
+	_ "github.com/henrylee2cn/pholcus_lib/zhihu_bianji"
+	_ "github.com/henrylee2cn/pholcus_lib/zhihu_daily"
 )
